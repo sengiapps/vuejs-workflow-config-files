89bd81ff728367f8316d2cec29aa361eff0e85d1
Daniel Sarmiento Cordero <danielsarcor@gmail.com>
2018-02-15 14:33:01 -0500

    - Se añadio resolve.webpack.js

---
+++ resolve.webpack.js
@@ -0,0 +1,16 @@
+const path = require('path');
+
+const resolve = {
+  alias: {
+    /* Resolve directories */
+    // Directory: path.resolve(__dirname, 'src/Directory'),
+
+    /* Resolve Files */
+    // file$: path.resolve(
+    //   __dirname,
+    //   'src/path/to/file.ext'
+    // ),
+  }
+};
+
+module.exports = resolve;
